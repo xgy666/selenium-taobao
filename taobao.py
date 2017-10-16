b64b42d23c3abc49ef4d00e601c181b83337c577
xgy666 <mrxue520@gmail.com>
2017-10-16 11:17:54 +0800

    Update taobao.py

--- taobao.py
+++ taobao.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait  # 专业版
+from selenium.webdriver.support.ui import WebDriverWait  
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import re
